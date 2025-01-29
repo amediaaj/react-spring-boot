2f72638b73973b9049bd3a5441a56f0f47797bcf
Alex Amedia <aamedia@asu.edu>
2025-01-29 14:58:34 -0700

    Creating React Project Template

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -3,10 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          We made our first React application!
-        </p>
+    <div>
+      <div>
+        <div>
+          Your Todos
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Feed dog</td>
+                <td>Cassidy</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Get haircut</td>
+                <td>Alex</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
