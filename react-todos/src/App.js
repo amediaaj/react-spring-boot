2cb6013ce0a65d21dbe41fc9be0e3b7c2e2b4557
Alex Amedia <aamedia@asu.edu>
2025-01-29 14:47:13 -0700

    Behind the Scenes of A React Project - Demo

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -4,15 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>
           We made our first React application!
         </p>
-      </header>
     </div>
   );
 }
